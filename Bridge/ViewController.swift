a056871c4ce463945cd6e30e8c8c633a1195d76b
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-21 14:42:32 +0900

    Initial Commit

---
+++ Bridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bridge
+//
+//  Created by 정호윤 on 2023/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
