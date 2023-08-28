b7f6f8eb3bd838833e94beceaf2b4916d7e34e3b
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-08-28 14:35:39 +0900

    feat: MainCoordinator 정의
    - MainCoordinator 정의
    - MainCoordinatorProtocol 정의
    - MainViewController 정의
    
    #6

---
+++ Bridge/Sources/Presenter/Tab/Main/Coordinator/MainCoordinator.swift
@@ -0,0 +1,48 @@
+//
+//  MainCoordinator.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/08/28.
+//
+
+import UIKit
+
+protocol MainCoordinatorProtocol: Coordinator {
+    func connectToNotificationView()
+    func connectToFilterView()
+    func connectToSearchView()
+    func connectToDetailView()
+    func connectToWriteView()
+}
+
+final class MainCoordinator: MainCoordinatorProtocol {
+    
+    weak var delegate: CoordinatorDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator]
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        self.childCoordinators = []
+    }
+    
+    func start() {
+        showMainViewController()
+    }
+}
+
+extension MainCoordinator {
+    func showMainViewController() {
+        let mainVC = MainViewController()
+        navigationController.pushViewController(mainVC, animated: true)
+    }
+}
+
+// MARK: - MainCoordinatorProtocol Method
+extension MainCoordinator {
+    func connectToNotificationView() { }
+    func connectToFilterView() { }
+    func connectToSearchView() { }
+    func connectToDetailView() { }
+    func connectToWriteView() { }
+}
