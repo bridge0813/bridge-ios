a7de83bb2000fce2410c0de39e90fa02ced87ae5
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-28 15:10:48 +0900

    feat: Use case 구현
    - FetchChatRoomsUseCase
    - LeaveChatRoomUseCase
    
    #7

---
+++ Bridge/Sources/Domain/UseCases/Chat/ChatRoom/FetchChatRoomsUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  FetchChatRoomsUseCase.swift
+//  Bridge
+//
+//  Created by 정호윤 on 2023/08/28.
+//
+
+import Foundation
+import RxSwift
+
+protocol FetchChatRoomsUseCase {
+    func execute() -> Observable<[ChatRoom]>
+}
+
+final class DefaultFetchChatRoomsUseCase: FetchChatRoomsUseCase {
+    
+    private let chatRoomRepository: ChatRoomRepository
+    
+    init(chatRoomRepository: ChatRoomRepository) {
+        self.chatRoomRepository = chatRoomRepository
+    }
+    
+    func execute() -> Observable<[ChatRoom]> {
+        chatRoomRepository.fetch()
+    }
+}
