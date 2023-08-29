eee1164049a7dbd6f1adae75f8859e4c204e4646
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-29 14:00:49 +0900

    feat: 네트워크 서비스 정의
    #11

---
+++ Bridge/Sources/Data/Network/NetworkService/Chat/ChatRoomNetworkService.swift
@@ -0,0 +1,52 @@
+//
+//  ChatService.swift
+//  Bridge
+//
+//  Created by 정호윤 on 2023/08/29.
+//
+
+import Foundation
+import RxSwift
+
+protocol ChatRoomNetworkService {
+    func request(_ endpoint: Endpoint) -> Observable<Data>
+    
+    func requestTestData() -> Observable<[ChatRoomDTO]>
+}
+
+final class DefaultChatRoomNetworkService: ChatRoomNetworkService {
+    
+    func request(_ endpoint: Endpoint) -> Observable<Data> {
+        guard let urlRequest = endpoint.toURLRequest() else { return .error(NetworkError.invalidURL) }
+        return URLSession.shared.rx.data(request: urlRequest).asObservable()
+    }
+    
+    func requestTestData() -> Observable<[ChatRoomDTO]> {
+        Observable.just([
+            ChatRoomDTO(
+                id: "1",
+                profileImage: nil,
+                name: "정호윤",
+                time: Date(),
+                messageType: .text,
+                messagePreview: "메시지 미리보기 1"
+            ),
+            ChatRoomDTO(
+                id: "2",
+                profileImage: nil,
+                name: "엄지호",
+                time: Date(),
+                messageType: .text,
+                messagePreview: "메시지 미리보기 2"
+            ),
+            ChatRoomDTO(
+                id: "3",
+                profileImage: nil,
+                name: "홍길동",
+                time: Date(),
+                messageType: .text,
+                messagePreview: "메시지 미리보기 3"
+            )
+        ])
+    }
+}
