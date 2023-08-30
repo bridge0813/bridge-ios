0d6eb6560712236ebfd18e9fcd3aef486950179b
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-30 18:23:41 +0900

    feat: BaseTableViewCell 정의
    #15

--- Bridge/Sources/Presenter/Common/Base/BaseTableViewCell.swift
+++ Bridge/Sources/Presenter/Common/Base/BaseTableViewCell.swift
@@ -16,10 +16,23 @@ class BaseTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         backgroundColor = .white
+        
+        configureAttributes()
+        configureUI()
+        bind()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    /// 기타 속성들을 설정하기 위한 메서드
+    func configureAttributes() { }
+    
+    /// UI와 관련된 속성들(뷰 계층, 레이아웃 등)을 설정하기 위한 메서드
+    func configureUI() { }
+    
+    /// 뷰 모델과 뷰를 바인딩하기 위한 메서드
+    func bind() { }
 }
