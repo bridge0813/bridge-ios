a7766f52f566fc00295062cf3f794301a7699fae
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-30 18:11:09 +0900

    feat: BaseViewController 정의
    #15

--- Bridge/Sources/Presenter/Common/Base/BaseViewController.swift
+++ Bridge/Sources/Presenter/Common/Base/BaseViewController.swift
@@ -21,9 +21,24 @@ class BaseViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    /// 기타 속성(데이터 소스, 네비게이션 바 등)들을 설정하기 위한 메서드
+    func configureAttributes() { }
+    
+    /// UI와 관련된 속성들(뷰 계층, 레이아웃 등)을 설정하기 위한 메서드
+    func configureUI() { }
+    
+    /// 뷰 모델과 뷰를 바인딩하기 위한 메서드
+    func bind() { }
+}
+
+// MARK: - Life cycles
+extension BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
+        
+        configureAttributes()
+        configureUI()
+        bind()
     }
 }
