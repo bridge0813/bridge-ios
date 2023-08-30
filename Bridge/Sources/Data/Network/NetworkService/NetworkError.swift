20b83e096eee58f125c092c58bb64f090b053863
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-30 11:52:46 +0900

    chore: 오타 수정
    #11

--- Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
+++ Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
@@ -13,7 +13,7 @@ enum NetworkError: Error {
 }
 
 extension NetworkError {
-    var dsecription: String? {
+    var description: String? {
         switch self {
         case .invalidURL: return "올바르지 않은 URL입니다."
         case .unknown:    return "알 수 없는 오류입니다."
