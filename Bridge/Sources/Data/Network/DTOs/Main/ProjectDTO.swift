de4229ff49ce4b87b513c59ab4c994ff9ddceba4
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-08-30 22:24:02 +0900

    feat: ProjectDTO -> Project 변환 구현
    #18

--- Bridge/Sources/Data/Network/DTOs/Main/ProjectDTO.swift
+++ Bridge/Sources/Data/Network/DTOs/Main/ProjectDTO.swift
@@ -18,4 +18,33 @@ struct ProjectDTO: Codable {
     let deadlineDate: Date  // 공고 모집마감 날짜
 }
 
-
+extension ProjectDTO {
+    
+    func toModel() -> Project {
+        let project = Project(
+            id: id,
+            title: title,
+            totalRequiredMembersCount: 0,
+            fields: fields,
+            dDayCount: Date().calculateDDay(to: deadlineDate),
+            requiredFieldTag: requiredFieldTag,
+            requiredStackTag: requiredStackTag,
+            startDate: startDate.toString(),
+            endDate: endDate.toString()
+        )
+        
+        let totalRequiredMembersCount = project.calculateTotalRequiredMembersCount()
+        
+        return Project(
+            id: project.id,
+            title: project.title,
+            totalRequiredMembersCount: totalRequiredMembersCount,
+            fields: project.fields,
+            dDayCount: project.dDayCount,
+            requiredFieldTag: project.requiredFieldTag,
+            requiredStackTag: project.requiredStackTag,
+            startDate: project.startDate,
+            endDate: project.endDate
+        )
+    }
+}
