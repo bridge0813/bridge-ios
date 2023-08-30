b71f9dab63eb947a83ba93cbaeb618c2a5be052a
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-30 18:23:24 +0900

    feat: BaseView 정의
    #15

---
+++ Bridge/Sources/Presenter/Common/Base/BaseView.swift
@@ -0,0 +1,38 @@
+//
+//  BaseView.swift
+//  Bridge
+//
+//  Created by 정호윤 on 2023/08/30.
+//
+
+import UIKit
+import RxSwift
+
+class BaseView: UIView {
+    
+    var disposeBag = DisposeBag()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        configureAttributes()
+        configureUI()
+        bind()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /// 기타 속성들을 설정하기 위한 메서드
+    func configureAttributes() { }
+    
+    /// UI와 관련된 속성들(뷰 계층, 레이아웃 등)을 설정하기 위한 메서드
+    func configureUI() { }
+    
+    /// 뷰 모델과 뷰를 바인딩하기 위한 메서드
+    func bind() { }
+}
