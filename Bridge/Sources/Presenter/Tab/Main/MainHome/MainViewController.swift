7de0aececb032e17ca7f6f8c105c842bf931f706
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-08-30 22:47:16 +0900

    feat: ViewController 수정
    - MainViewModel 주입
    - MainViewModel의 transform()
    
    #18

--- Bridge/Sources/Presenter/Tab/Main/MainHome/MainViewController.swift
+++ Bridge/Sources/Presenter/Tab/Main/MainHome/MainViewController.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class MainViewController: UIViewController {
-
+final class MainViewController: UIViewController {
+    let viewModel: MainViewModel
+    
+    init(viewModel: MainViewModel) {
+        self.viewModel = viewModel
+        let n = viewModel.transform(input: MainViewModel.Input(viewDidLoadTrigger: PublishRelay<Void>()))
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .green
     }
-
 }
