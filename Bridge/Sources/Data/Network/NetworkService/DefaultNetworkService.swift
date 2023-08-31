ba15ce7f60dda0dc86737cc3a051a6c231cd9640
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-08-31 20:41:19 +0900

    feat: NetworkService 구현
    #21

--- Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
+++ Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
@@ -15,7 +15,60 @@ final class DefaultNetworkService: NetworkService {
         return URLSession.shared.rx.data(request: urlRequest).asObservable()
     }
     
-    func requestTestData() -> Observable<[ChatRoomDTO]> {
+    // MARK: - For test
+    func requestTestChatRoom(id: String) -> Observable<ChatRoomDTO> {
+        switch id {
+        case "1":
+            return Observable.just(
+                ChatRoomDTO(
+                    id: "1",
+                    profileImage: nil,
+                    name: "정호윤",
+                    time: Date(),
+                    messageType: .text,
+                    messagePreview: "메시지 미리보기 1"
+                )
+            )
+            
+        case "2":
+            return Observable.just(
+                ChatRoomDTO(
+                    id: "2",
+                    profileImage: nil,
+                    name: "엄지호",
+                    time: Date(),
+                    messageType: .text,
+                    messagePreview: "메시지 미리보기 2"
+                )
+            )
+            
+        case "3":
+            return Observable.just(
+                ChatRoomDTO(
+                    id: "3",
+                    profileImage: nil,
+                    name: "홍길동",
+                    time: Date(),
+                    messageType: .text,
+                    messagePreview: "메시지 미리보기 3"
+                )
+            )
+            
+        default:
+            return Observable.just(
+                ChatRoomDTO(
+                    id: "-1",
+                    profileImage: nil,
+                    name: "기타",
+                    time: Date(),
+                    messageType: .text,
+                    messagePreview: "기타"
+                )
+            )
+        }
+    }
+    
+    func requestTestChatRooms() -> Observable<[ChatRoomDTO]> {
         Observable.just([
             ChatRoomDTO(
                 id: "1",
@@ -40,6 +93,14 @@ final class DefaultNetworkService: NetworkService {
                 time: Date(),
                 messageType: .text,
                 messagePreview: "메시지 미리보기 3"
+            ),
+            ChatRoomDTO(
+                id: "4",
+                profileImage: nil,
+                name: "홍길동",
+                time: Date(),
+                messageType: .text,
+                messagePreview: "메시지 미리보기 4"
             )
         ])
     }
