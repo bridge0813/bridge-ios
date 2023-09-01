cec82f1e575ec2ea01e710c6ada549672a44b362
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-01 13:41:49 +0900

    feat: HotProjectCollectionViewCell 생성(레이아웃 전)
    - UI 프로퍼티 설정
    
    #22

--- Bridge/Sources/Presenter/Tab/Main/MainHome/HotProjectCollectionViewCell.swift
+++ Bridge/Sources/Presenter/Tab/Main/MainHome/HotProjectCollectionViewCell.swift
@@ -6,7 +6,74 @@
 //
 
 import UIKit
+import FlexLayout
+import PinLayout
+import RxCocoa
+import RxSwift
 
 final class HotProjectCollectionViewCell: BaseCollectionViewCell {
+    // MARK: - Properties
+    private let projectBackgroundView = UIView()
     
+    private let titleLabel = UILabel()
+    private let dDayLabel = UILabel()
+    
+    private let personImageView = UIImageView()
+    private let recruitsLabel = UILabel()
+
+    func bind(_ chatRoom: Driver<Project>) {
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = ""
+        dDayLabel.text = ""
+        recruitsLabel.text = ""
+        disposeBag = DisposeBag()
+    }
+    
+    // MARK: - Configurations
+    override func configureLayouts() {
+        addSubview(projectBackgroundView)
+        projectBackgroundView.backgroundColor = .black
+        projectBackgroundView.layer.cornerRadius = 10
+        projectBackgroundView.clipsToBounds = true
+        
+        titleLabel.textColor = .black
+        titleLabel.font = .boldSystemFont(ofSize: 16)
+        titleLabel.numberOfLines = 2
+        
+        dDayLabel.textColor = .darkGray
+        dDayLabel.font = .systemFont(ofSize: 13.5)
+        dDayLabel.clipsToBounds = true
+        dDayLabel.layer.cornerRadius = 13
+        dDayLabel.layer.borderColor = UIColor.darkGray.cgColor
+        dDayLabel.layer.borderWidth = 1
+        
+        personImageView.image = UIImage(systemName: "person.fill")
+        personImageView.backgroundColor = .clear
+        personImageView.tintColor = .darkGray
+        
+        recruitsLabel.textColor = .darkGray
+        recruitsLabel.font = .systemFont(ofSize: 13)
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        projectBackgroundView.pin.all()
+        projectBackgroundView.flex.layout()
+    }
 }
+
+// MARK: - Configuration
+//private extension ChatRoomCell {
+//    func configureCell(with chatRoom: ChatRoom) {
+//        nameLabel.text = chatRoom.name
+//        timeLabel.text = chatRoom.time.formatted()
+//        messagePreviewLabel.text = chatRoom.messagePreview
+//    }
+//}
