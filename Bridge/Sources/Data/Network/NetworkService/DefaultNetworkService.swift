2140ae19505418909a8917e937a0825d31fee998
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-01 18:08:35 +0900

    feat: 테스트를 위한 DefaultNetworkService 수정
    - 여러 데이터를 넣어줌
    
    #22

--- Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
+++ Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
@@ -47,17 +47,47 @@ final class DefaultNetworkService: NetworkService {
     func requestTestProjectsData() -> Observable<[ProjectDTO]> {
         let currentDate = Date()
         
-        let projectDTO = ProjectDTO(
-            id: "1",
-            title: "개발자, 디자이너 구합니다",
-            numberOfRecruits: 6,
-            recruitmentField: ["개발자", "디자이너"],
-            techStackTags: ["iOS", "BackEnd", "UI/UX"],
-            startDate: currentDate,
-            endDate: currentDate,
-            deadlineDate: currentDate
-        )
-        
-        return Observable.just([projectDTO])
+        return Observable.just([
+            ProjectDTO(
+                id: "1",
+                title: "모임 플랫폼 디자이너 구합니다",
+                numberOfRecruits: 1,
+                recruitmentField: ["개발자", "디자이너"],
+                techStackTags: ["iOS", "BackEnd", "UI/UX"],
+                startDate: currentDate,
+                endDate: currentDate,
+                deadlineDate: currentDate
+            ),
+            ProjectDTO(
+                id: "2",
+                title: "웹 사이트 디자이너 구해요!!",
+                numberOfRecruits: 1,
+                recruitmentField: ["개발자", "디자이너"],
+                techStackTags: ["iOS", "BackEnd", "UI/UX"],
+                startDate: currentDate,
+                endDate: currentDate,
+                deadlineDate: currentDate
+            ),
+            ProjectDTO(
+                id: "3",
+                title: "개발자, 디자이너 구합니다",
+                numberOfRecruits: 6,
+                recruitmentField: ["개발자", "디자이너"],
+                techStackTags: ["iOS", "BackEnd", "UI/UX"],
+                startDate: currentDate,
+                endDate: currentDate,
+                deadlineDate: currentDate
+            ),
+            ProjectDTO(
+                id: "4",
+                title: "iOS 개발자 구합니다",
+                numberOfRecruits: 4,
+                recruitmentField: ["개발자", "디자이너"],
+                techStackTags: ["iOS", "BackEnd", "UI/UX"],
+                startDate: currentDate,
+                endDate: currentDate,
+                deadlineDate: currentDate
+            )
+        ])
     }
 }
