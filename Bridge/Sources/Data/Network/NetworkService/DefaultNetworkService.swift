9629dc98054059483617f1dc8c06baa9a8de9b42
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-02 18:36:19 +0900

    feat: requestTestProjectsData() 테스트 데이터 갯수 수정
    #26

--- Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
+++ Bridge/Sources/Data/Network/NetworkService/DefaultNetworkService.swift
@@ -124,50 +124,6 @@ final class DefaultNetworkService: NetworkService {
                 endDate: currentDate,
                 deadlineDate: currentDate,
                 scrapCount: 4
-            ),
-            ProjectDTO(
-                id: "4",
-                title: "iOS 개발자 구합니다",
-                numberOfRecruits: 4,
-                recruitmentField: ["개발자", "디자이너"],
-                techStackTags: ["iOS", "BackEnd", "UI/UX"],
-                startDate: currentDate,
-                endDate: currentDate,
-                deadlineDate: currentDate,
-                scrapCount: 3
-            ),
-            ProjectDTO(
-                id: "1",
-                title: "모임 플랫폼 디자이너 구합니다",
-                numberOfRecruits: 1,
-                recruitmentField: ["개발자", "디자이너"],
-                techStackTags: ["iOS", "BackEnd", "UI/UX"],
-                startDate: currentDate,
-                endDate: currentDate,
-                deadlineDate: currentDate,
-                scrapCount: 2
-            ),
-            ProjectDTO(
-                id: "2",
-                title: "웹 사이트 디자이너 구해요!!",
-                numberOfRecruits: 1,
-                recruitmentField: ["개발자", "디자이너"],
-                techStackTags: ["iOS", "BackEnd", "UI/UX"],
-                startDate: currentDate,
-                endDate: currentDate,
-                deadlineDate: currentDate,
-                scrapCount: 1
-            ),
-            ProjectDTO(
-                id: "3",
-                title: "개발자, 디자이너 구합니다",
-                numberOfRecruits: 6,
-                recruitmentField: ["개발자", "디자이너"],
-                techStackTags: ["iOS", "BackEnd", "UI/UX"],
-                startDate: currentDate,
-                endDate: currentDate,
-                deadlineDate: currentDate,
-                scrapCount: 0
             )
         ])
     }
