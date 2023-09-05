e42f43df980fe767f196d843286197d5508ede72
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-05 17:31:04 +0900

    remove: ProjectItems의 hash(into:) 메서드 삭제
    #29

--- Bridge/Sources/Domain/Entities/Main/ProjectItems.swift
+++ Bridge/Sources/Domain/Entities/Main/ProjectItems.swift
@@ -7,21 +7,7 @@
 
 import Foundation
 
-enum ProjectItems {
+enum ProjectItems: Hashable {
     case hot(HotProject)
     case main(Project)
-}
-
-extension ProjectItems: Hashable {
-    func hash(into hasher: inout Hasher) {
-        switch self {
-        case .hot(let hotProject):
-            hasher.combine("hot")
-            hasher.combine(hotProject)
-            
-        case .main(let project):
-            hasher.combine("main")
-            hasher.combine(project)
-        }
-    }
 }
