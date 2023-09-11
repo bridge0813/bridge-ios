db2d085a951314da102c29661fa4d7be9c051df3
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-11 18:48:25 +0900

    feat: CompletionViewModel 및 CompletionViewController 생성
    #49

---
+++ Bridge/Sources/Presentation/Tab/Main/CreateProject/Completion/CompletionViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CompletionViewController.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/09/11.
+//
+
+import UIKit
+import FlexLayout
+import PinLayout
+
+final class CompletionViewController: BaseViewController {
+    // MARK: - Properties
+    private let rootFlexContainer = UIView()
+   
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다음", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        
+        return button
+    }()
+
+    private let viewModel: CompletionViewModel
+    
+    // MARK: - Initializer
+    init(viewModel: CompletionViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureNavigationUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        rootFlexContainer.pin.all(view.pin.safeArea).marginTop(10)
+        rootFlexContainer.flex.layout()
+    }
+    
+    // MARK: - Methods
+    private func configureNavigationUI() {
+        navigationItem.title = "모집글 작성(완료)"
+    }
+    
+    override func configureLayouts() {
+        view.addSubview(rootFlexContainer)
+        rootFlexContainer.flex.direction(.column).padding(5).alignItems(.center).define { flex in
+            flex.addItem(nextButton).marginTop(100).width(50).height(50)
+        }
+    }
+    
+    override func configureAttributes() {
+        configureNavigationUI()
+    }
+    
+    override func bind() {
+        
+    }
+}
