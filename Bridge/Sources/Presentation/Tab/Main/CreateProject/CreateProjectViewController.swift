0bbc02f2e8033cfe304b9d83dc7825a9091421d3
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-11 15:44:26 +0900

    feat: CreateProject 뷰컨 및 뷰모델 생성
    #49

---
+++ Bridge/Sources/Presentation/Tab/Main/CreateProject/CreateProjectViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CreateProjectViewController.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/09/11.
+//
+
+import UIKit
+import FlexLayout
+import PinLayout
+
+final class CreateProjectViewController: BaseViewController {
+    // MARK: - Properties
+    private let rootFlexContainer = UIView()
+   
+    private let nextButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("다음", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        
+        return button
+    }()
+
+    private let viewModel: CreateProjectViewModel
+    
+    // MARK: - Initializer
+    init(viewModel: CreateProjectViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        configureNavigationUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        rootFlexContainer.pin.all(view.pin.safeArea).marginTop(10)
+        rootFlexContainer.flex.layout()
+    }
+    
+    // MARK: - Methods
+    private func configureNavigationUI() {
+        navigationItem.title = "모집글 작성"
+    }
+    
+    override func configureLayouts() {
+        view.addSubview(rootFlexContainer)
+        rootFlexContainer.flex.direction(.column).padding(5).alignItems(.center).define { flex in
+            flex.addItem(nextButton).marginTop(100).width(50).height(50)
+        }
+    }
+    
+    override func configureAttributes() {
+        configureNavigationUI()
+    }
+    
+    override func bind() {
+        
+    }
+}
---
+++ Bridge/Sources/Presentation/Tab/Main/CreateProject/CreateProjectViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  CreateProjectViewModel.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/09/11.
+//
+
+import RxSwift
+
+final class CreateProjectViewModel: ViewModelType {
+    // MARK: - Nested Types
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    // MARK: - Properties
+    let disposeBag = DisposeBag()
+    
+    
+    // MARK: - Initializer
+    init(
+        
+    ) {
+        
+    }
+    
+    // MARK: - Methods
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
