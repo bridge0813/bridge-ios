c603ded07a1bcbd78db06a4946bdf77b34921e6e
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-09-11 22:26:04 +0900

    feat: 코디네이터 델리게이트에 sign in 메서드 추가
    - showSignInViewController 메서드 추가
    
    #42

---
+++ Bridge/Sources/App/Coordinator/Coordinator.swift
@@ -0,0 +1,36 @@
+//
+//  Coordinator.swift
+//  Bridge
+//
+//  Created by 정호윤 on 2023/08/25.
+//
+
+import UIKit
+
+protocol CoordinatorDelegate: AnyObject {
+    func didFinish(childCoordinator: Coordinator)
+    
+    func showSignInViewController()
+}
+
+extension CoordinatorDelegate {
+    func showSignInViewController() { }
+}
+
+protocol Coordinator: AnyObject {
+    var delegate: CoordinatorDelegate? { get set }
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+    
+    init(navigationController: UINavigationController)
+    
+    func start()
+    func finish()
+}
+
+extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        delegate?.didFinish(childCoordinator: self)
+    }
+}
