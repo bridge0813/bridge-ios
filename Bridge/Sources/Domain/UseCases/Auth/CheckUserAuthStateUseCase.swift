32ead94ba747dd40d0e34fd8e88c1ff5d5debe1b
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-09-16 15:17:56 +0900

    feat: 도메인 레이어 구현
    #60

--- Bridge/Sources/Domain/UseCases/Auth/CheckUserAuthStateUseCase.swift
+++ Bridge/Sources/Domain/UseCases/Auth/CheckUserAuthStateUseCase.swift
@@ -7,26 +7,6 @@
 
 import RxSwift
 
-protocol CheckUserAuthStateUseCase {
-    func execute() -> Observable<UserAuthState>
-}
-
-enum UserAuthState {
-    case signedIn
-    case notSignedIn
-    case error
-}
-
-final class DefaultCheckUserAuthStateUseCase: CheckUserAuthStateUseCase {
-    
-    func execute() -> Observable<UserAuthState> {
-        Observable.combineLatest(checkAppleSignInState(), checkKakaoSignInState())
-            .map { appleSignInState, kakaoSignInState in
-                appleSignInState == .signedIn || kakaoSignInState == .signedIn ? .signedIn : .notSignedIn
-            }
-    }
-}
-
 // TODO: 유저 인증 상태 체크 플로우
 // 1. 로그인 기능이 필요한 기능 (프로젝트 지원하기 등)을 시도 - view model
 // 2. 로그인 상태 체크 (헤더에 토큰 넣어서 서버로 요청 - 토큰 없으면 빈값이 들어가거나 안들어감)
@@ -35,12 +15,19 @@ final class DefaultCheckUserAuthStateUseCase: CheckUserAuthStateUseCase {
 //     4-1. 로그인 된 상태 -> 토큰들 키체인에 저장 후 그대로 진행
 //     4-2. 로그인 or 회원가입 필요한 상태 -> 로그인 창 띄우기
 
-private extension DefaultCheckUserAuthStateUseCase {
-    private func checkAppleSignInState() -> Observable<UserAuthState> {
-        .just(.notSignedIn)  // 예시
+protocol CheckUserAuthStateUseCase {
+    func execute() -> Observable<UserAuthState>
+}
+
+final class DefaultCheckUserAuthStateUseCase: CheckUserAuthStateUseCase {
+    
+    private let authRepository: AuthRepository
+    
+    init(authRepository: AuthRepository) {
+        self.authRepository = authRepository
     }
     
-    private func checkKakaoSignInState() -> Observable<UserAuthState> {
-        .just(.notSignedIn)  // 예시
+    func execute() -> Observable<UserAuthState> {
+        authRepository.checkUserAuthState()
     }
 }
