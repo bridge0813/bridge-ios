0498971a5bda747c95c89bc4bc1a6b8a9e6d21f1
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-09-23 21:44:27 +0900

    chore: 네트워크 에러 case 추가
    #64

--- Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
+++ Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
@@ -7,14 +7,25 @@
 
 enum NetworkError: Error {
     case invalidURL
+    case statusCode(Int)
+    case decodingFailed
     case unknown
 }
 
 extension NetworkError {
     var description: String? {
         switch self {
-        case .invalidURL: return "올바르지 않은 URL입니다."
-        case .unknown:    return "알 수 없는 오류입니다."
+        case .invalidURL:
+            return "올바르지 않은 URL입니다."
+            
+        case .statusCode(let statusCode):
+            return "상태코드: \(statusCode)"
+            
+        case .decodingFailed:
+            return "디코딩에 실패했습니다."
+            
+        case .unknown:
+            return "알 수 없는 오류입니다."
         }
     }
 }
