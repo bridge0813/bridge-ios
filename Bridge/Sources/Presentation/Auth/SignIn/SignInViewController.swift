9068deab7fdb27ecd161645081db96bbdccbb3f5
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-09-25 23:00:59 +0900

    design: 애플 로그인 버튼 구현
    #66

--- Bridge/Sources/Presentation/Auth/SignIn/SignInViewController.swift
+++ Bridge/Sources/Presentation/Auth/SignIn/SignInViewController.swift
@@ -18,10 +18,16 @@ final class SignInViewController: BaseViewController {
     private let rootFlexViewContainer = UIView()
     
     private let signInWithAppleButton: UIButton = {
-        let button = UIButton(configuration: .filled())
-        button.setTitle("Apple로 로그인", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        return button
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .black
+        configuration.attributedTitle = AttributedString(
+            "Apple로 로그인",
+            attributes: AttributeContainer([.font: BridgeFont.button1.font, .foregroundColor: UIColor.white])
+        )
+        configuration.image = UIImage(systemName: "apple.logo")
+        configuration.imagePadding = 8
+        configuration.imagePlacement = .leading
+        return UIButton(configuration: configuration)
     }()
 
     private let viewModel: SignInViewModel
@@ -44,13 +50,9 @@ final class SignInViewController: BaseViewController {
     override func configureLayouts() {
         view.addSubview(rootFlexViewContainer)
         
-        rootFlexViewContainer.flex
-            .direction(.column)
-            .justifyContent(.center)
-            .alignItems(.center)
-            .define { flex in
-                flex.addItem(signInWithAppleButton).width(343).height(45)
-            }
+        rootFlexViewContainer.flex.direction(.column).justifyContent(.center).alignItems(.center).define { flex in
+            flex.addItem(signInWithAppleButton).width(343).height(45)
+        }
     }
     
     override func viewDidLayoutSubviews() {
