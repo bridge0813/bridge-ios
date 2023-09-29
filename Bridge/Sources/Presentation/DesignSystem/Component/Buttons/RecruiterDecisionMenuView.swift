2d8aa2feddbac8e932303efbea8179a9ec517a7a
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-09-29 13:47:20 +0900

    refactor: layer에 접근하지 않고, DividerView를 사용하는 방식으로 변경
    #71

--- Bridge/Sources/Presentation/DesignSystem/Component/Buttons/RecruiterDecisionMenuView.swift
+++ Bridge/Sources/Presentation/DesignSystem/Component/Buttons/RecruiterDecisionMenuView.swift
@@ -17,45 +17,35 @@ final class RecruiterDecisionMenuView: BaseView {
     let refuseButton = BridgeBlockButton(with: "거절하기", style: .confirm)
     
     override func configureLayouts() {
-        applyCornerStyles()
-    
+        let leftDivider = createDividerView()
+        let rightDivider = createDividerView()
+        
         addSubview(rootFlexContainer)
         rootFlexContainer.flex.direction(.row).define { flex in
             flex.addItem(chatButton).grow(1).height(48)
+            flex.addItem(leftDivider).width(0.3).height(28).alignSelf(.center)
             flex.addItem(acceptButton).grow(1).height(48).cornerRadius(0)
+            flex.addItem(rightDivider).width(0.3).height(28).alignSelf(.center)
             flex.addItem(refuseButton).grow(1).height(48)
         }
+        
+        applyCornerStyles()
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         rootFlexContainer.pin.all()
         rootFlexContainer.flex.layout()
-        acceptButton.layer.addDividerBorders([.left, .right])
     }
     
     private func applyCornerStyles() {
         chatButton.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
         refuseButton.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
     }
-}
-
-extension CALayer {
-    // 뷰의 layer 왼쪽, 오른쪽 위치에 원하는 크기만큼 보더라인을 그려주는 메서드.
-    func addDividerBorders(_ edges: [UIRectEdge]) {
-        for edge in edges {
-            let border = CALayer()
-            let borderWidth: CGFloat = 0.3
-            let borderHeight: CGFloat = 28
-
-            let verticalOffset = (frame.height - borderHeight) / 2  // 구분선이 가운데에 위치하도록
-            let leftBorderFrame = CGRect(x: 0, y: verticalOffset, width: borderWidth, height: borderHeight)
-            let rightBorderFrame = CGRect(x: frame.width - borderWidth, y: verticalOffset, width: 0.3, height: borderHeight)
-            
-            border.frame = edge == .left ? leftBorderFrame : rightBorderFrame
-            border.backgroundColor = UIColor.white.cgColor
-            
-            self.addSublayer(border)
-        }
+    
+    private func createDividerView() -> UIView {
+        let divider = UIView()
+        divider.backgroundColor = UIColor.white
+        return divider
     }
 }
