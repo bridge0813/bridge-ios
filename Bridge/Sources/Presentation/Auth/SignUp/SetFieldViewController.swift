4c6d0210a6e052d8abf75d36c333161b18febd78
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-04 20:21:40 +0900

    refactor: magic number 최소화
    #76

--- Bridge/Sources/Presentation/Auth/SignUp/SetFieldViewController.swift
+++ Bridge/Sources/Presentation/Auth/SignUp/SetFieldViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 import PinLayout
 import FlexLayout
-import RxCocoa
 import RxSwift
 
 final class SetFieldViewController: BaseViewController {
@@ -87,57 +86,63 @@ final class SetFieldViewController: BaseViewController {
         navigationController?.interactivePopGestureRecognizer?.isEnabled = false  // 밀어서 뒤로가기 제한
     }
     
-    // MARK: - description label line height, 유지보수성, 동적으로? 다듬기
+    // MARK: - description label line height
     override func configureLayouts() {
         view.addSubview(rootFlexViewContainer)
         
+        let labelMargin: CGFloat = 14           // 레이블과 다른 컴포넌트 간 마진
+        let sectionMargin: CGFloat = 24         // 섹션 간 마진
+        let containerSize: CGFloat = 200        // 관심분야 버튼 컨테이너 크기
+        let buttonMargin: CGFloat = 12          // 관심분야 버튼 간 간격
+        
         rootFlexViewContainer.flex.paddingHorizontal(16).define { flex in
-            flex.addItem(descriptionLabel).marginTop(24)
-            flex.addItem(tipMessageBox).marginTop(14).marginBottom(40)
+            flex.addItem(descriptionLabel).marginTop(24).marginBottom(labelMargin)
+            flex.addItem(tipMessageBox).marginBottom(40)
             
             // 개발자 섹션
             flex.addItem().define { flex in
-                flex.addItem(developerLabel).marginBottom(14)
+                flex.addItem(developerLabel).marginBottom(labelMargin)
                 
                 flex.addItem()
                     .direction(.row)
                     .alignItems(.start)
                     .wrap(.wrap)
-                    .width(190)
-                    .marginBottom(24)
+                    .width(containerSize)
+                    .marginBottom(sectionMargin)
                     .define { flex in
-                        flex.addItem(iosButton).marginRight(12).marginBottom(12)
+                        flex.addItem(iosButton).marginRight(buttonMargin).marginBottom(buttonMargin)
                         flex.addItem(androidButton)
-                        flex.addItem(frontendButton).marginRight(12).marginBottom(12)
+                        flex.addItem(frontendButton).marginRight(buttonMargin).marginBottom(buttonMargin)
                         flex.addItem(backendButton)
                     }
             }
             
             // 디자이너 섹션
             flex.addItem().define { flex in
-                flex.addItem(designerLabel).marginBottom(14)
+                flex.addItem(designerLabel).marginBottom(labelMargin)
                 
                 flex.addItem()
                     .direction(.row)
                     .alignItems(.start)
                     .wrap(.wrap)
-                    .width(165)
-                    .marginBottom(24)
+                    .width(containerSize)
+                    .marginBottom(sectionMargin)
                     .define { flex in
-                        flex.addItem(uiuxButton).marginRight(12).marginBottom(12)
+                        flex.addItem(uiuxButton).marginRight(buttonMargin).marginBottom(buttonMargin)
                         flex.addItem(bibxButton)
                         flex.addItem(videomotionButton)
                     }
             }
             
-            // PM 섹션
+            // 기획자 섹션
             flex.addItem().define { flex in
-                flex.addItem(pmLabel).marginBottom(14)
+                flex.addItem(pmLabel).marginBottom(labelMargin)
                 
                 flex.addItem()
                     .direction(.row)
                     .alignItems(.start)
                     .wrap(.wrap)
+                    .width(containerSize)
                     .define { flex in
                         flex.addItem(pmButton)
                     }
