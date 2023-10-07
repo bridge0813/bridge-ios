8595587f2cd807e7a9a3904eaab3854bdb84442f
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-07 15:21:20 +0900

    refactor: json 필드에 nil 값 명시적으로 전달하도록 수정
    #79

--- Bridge/Sources/Data/Network/DTOs/Auth/SignUp/SignUpRequestDTO.swift
+++ Bridge/Sources/Data/Network/DTOs/Auth/SignUp/SignUpRequestDTO.swift
@@ -14,7 +14,8 @@ extension SignUpRequestDTO: Encodable {
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
-        try container.encode(userID, forKey: .userID)
+        if userID != nil { try container.encode(userID, forKey: .userID) }
+        else { try container.encodeNil(forKey: .userID) }  // 필드에 nil 값 명시적으로 인코딩
         
         let userFields = ["fieldName": selectedFields]
         try container.encode(userFields, forKey: .userField)
