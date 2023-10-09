954cce05b5972026fd3873f6d1ed3f27711a0d7d
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-09 15:39:29 +0900

    refactor: 리포지토리 개선
    #79

--- Bridge/Sources/Data/Repositories/Auth/DefaultAuthRepository.swift
+++ Bridge/Sources/Data/Repositories/Auth/DefaultAuthRepository.swift
@@ -6,43 +6,50 @@
 //
 
 import RxSwift
+import Foundation
 
 final class DefaultAuthRepository: AuthRepository {
     
     private let networkService: NetworkService
     private let tokenStorage: TokenStorage
     
-    init(networkService: NetworkService, tokenStorage: TokenStorage) {
+    init(networkService: NetworkService, tokenStorage: TokenStorage = KeychainTokenStorage()) {
         self.networkService = networkService
         self.tokenStorage = tokenStorage
     }
     
     func signInWithApple(credentials: UserCredentials) -> Single<SignInResult> {
-        let userName = tokenStorage.fetchToken(for: KeychainAccount.userName)
-        
         if !credentials.name.isEmpty {  // 이름은 2번째 로그인부터 빈 문자열이 리턴되므로, 빈 문자열 저장하지 않도록 처리
-            tokenStorage.saveToken(credentials.name, for: KeychainAccount.userName)
+            tokenStorage.saveToken(credentials.name, for: .userName)
         }
         
-        return networkService.signInWithAppleTest(userName: userName, credentials: credentials)
-            .flatMap { [weak self] signInResponseDTO in
-                // 응답 저장
-                self?.tokenStorage.saveToken(signInResponseDTO.accessToken, for: KeychainAccount.accessToken)
-                self?.tokenStorage.saveToken(signInResponseDTO.refreshToken, for: KeychainAccount.refreshToken)
-                self?.tokenStorage.saveToken(String(signInResponseDTO.userId), for: KeychainAccount.userID)
-                
-                // SignInResponseDTO를 SignInResult로 매핑
-                let result: SignInResult = signInResponseDTO.isRegistered ? .success : .needSignUp
-                
-                return Single.just(result)
+        let signInWithAppleRequestDTO = SignInWithAppleRequestDTO(
+            userName: tokenStorage.fetchToken(for: .userName) ?? "",
+            identityToken: credentials.identityToken ?? ""
+        )
+        let authEndpoint = AuthEndpoint.signInWithApple(requestDTO: signInWithAppleRequestDTO)
+        
+        return networkService.request(authEndpoint)
+            .map { [weak self] signInResponseDTO -> SignInResult in
+                self?.storeToken(signInResponseDTO)  // 응답 저장
+                return signInResponseDTO.isRegistered ? .success : .needSignUp
             }
             .catch { _ in
                 Single.just(SignInResult.failure)
             }
     }
     
+    private func storeToken(_ signInResponseDTO: SignInResponseDTO) {
+        tokenStorage.saveToken(signInResponseDTO.accessToken, for: .accessToken)
+        tokenStorage.saveToken(signInResponseDTO.refreshToken, for: .refreshToken)
+        tokenStorage.saveToken(String(signInResponseDTO.userId), for: .userID)
+    }
+    
     func signUp(selectedFields: [String]) -> Single<Void> {
-        let userID = Int(tokenStorage.fetchToken(for: KeychainAccount.userID) ?? "")
-        return networkService.signUpTest(userID: userID, selectedFields: selectedFields)
+        let userID = Int(tokenStorage.fetchToken(for: .userID) ?? "")
+        let signUpRequestDTO = SignUpRequestDTO(userID: userID, selectedFields: selectedFields)
+        let authEndpoint = AuthEndpoint.signUp(requestDTO: signUpRequestDTO)
+
+        return networkService.request(authEndpoint)
     }
 }
