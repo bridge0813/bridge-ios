0d37be0ea1f50568c4d84706dc2e0dc5752ed5b8
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-13 16:49:12 +0900

    refactor: 네트워크 에러 개선
    #83

--- Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
+++ Bridge/Sources/Data/Network/NetworkService/NetworkError.swift
@@ -8,44 +8,41 @@
 import Foundation
 
 enum NetworkError: Error {
-    case invalidURL
-    
-    /// 기대하지 않은 응답 타입일 때 (e.g. HTTPURLResponse가 아닐 때)
-    case invalidResponseType
-    
-    case decodingFailed
-    
+    case invalidRequest
+    case invalidResponse
+    case unauthorized
     case statusCode(Int)
-    
     case underlying(Error)
-    
     case unknown
 }
 
-extension NetworkError: LocalizedError {
-    static let statusMessages = [
-        401: "Unauthorized"
-    ]
-    
-    var errorDescription: String? {
-        switch self {
-        case .invalidURL:
-            return "invalid URL"
-            
-        case .invalidResponseType:
-            return "received invalid response type"
-            
-        case .decodingFailed:
-            return "failed to decode response"
-            
-        case .statusCode(let statusCode):
-            return NetworkError.statusMessages[statusCode] ?? "undeclared status code: \(statusCode) error"
-            
-        case .underlying(let error):
-            return error.localizedDescription
-            
-        case .unknown:
-            return "unknown error occured"
-        }
-    }
-}
+//extension NetworkError: LocalizedError {
+//    static let statusMessages = [
+//        401: "Unauthorized"
+//    ]
+//
+//    var errorDescription: String? {
+//        switch self {
+//        case .invalidRequest:
+//            return "invalid URLRequest"
+//
+//        case .invalidResponse:
+//            return "received invalid response type"
+//
+//        case .unauthorized:
+//            return "unauthorized user"
+//
+//        case .decodingFailed:
+//            return "failed to decode response"
+//
+//        case .statusCode(let statusCode):
+//            return NetworkError.statusMessages[statusCode] ?? "undeclared status code: \(statusCode) error"
+//
+//        case .underlying(let error):
+//            return error.localizedDescription
+//
+//        case .unknown:
+//            return "unknown error occured"
+//        }
+//    }
+//}
