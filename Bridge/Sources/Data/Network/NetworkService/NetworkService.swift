487cc1922618380a52ba4cc25c595b2d2bc154df
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-13 16:48:50 +0900

    feat: 네트워크 서비스 개선
    #82

--- Bridge/Sources/Data/Network/NetworkService/NetworkService.swift
+++ Bridge/Sources/Data/Network/NetworkService/NetworkService.swift
@@ -8,23 +8,13 @@
 import Foundation
 import RxSwift
 
-typealias NetworkService = BasicNetworkService & ProjectNetworkService & ChatNetworkService
-
-protocol BasicNetworkService {
-    /// response body에 데이터가 없고, 성공 여부만 반환하는 함수
-    func request(_ endpoint: Endpoint) -> Single<Void>
-    
-    /// response body에 데이터가 있고, 해당 데이터를 디코딩해 반환하는 함수
-    func request<T: Decodable>(_ endpoint: Endpoint) -> Single<T>
+protocol NetworkService: ProjectNetworkService {
+    /// Interceptor가 필요 없는 경우 nil을 할당
+    func request(_ endpoint: Endpoint, interceptor: Interceptor?) -> Observable<Data>
 }
 
-// TODO: 아래 함수들 제거
+// TODO: 프로토콜 제거 및 network service의 상속 제거
 protocol ProjectNetworkService {
     func requestTestProjectsData() -> Observable<[ProjectDTO]>
     func requestTestHotProjectsData() -> Observable<[ProjectDTO]>
-}
-
-protocol ChatNetworkService {
-    func requestTestChatRooms() -> Observable<[ChatRoomDTO]>
-    func leaveChatRoom(id: String) -> Single<Void>
 }
