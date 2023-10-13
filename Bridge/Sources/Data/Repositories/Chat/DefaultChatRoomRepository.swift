e26beec6625d68efc847085785c1c5f649f7c157
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-13 21:57:17 +0900

    refactor: 리포지토리 개선
    #83

--- Bridge/Sources/Data/Repositories/Chat/DefaultChatRoomRepository.swift
+++ Bridge/Sources/Data/Repositories/Chat/DefaultChatRoomRepository.swift
@@ -5,26 +5,34 @@
 //  Created by 정호윤 on 2023/08/28.
 //
 
+import Foundation
 import RxSwift
 
 final class DefaultChatRoomRepository: ChatRoomRepository {
     
     private let networkService: NetworkService
+    private let tokenStorage: TokenStorage
     
-    init(networkService: NetworkService) {
+    init(networkService: NetworkService, tokenStorage: TokenStorage = KeychainTokenStorage()) {
         self.networkService = networkService
+        self.tokenStorage = tokenStorage
     }
     
     func fetchChatRooms() -> Observable<[ChatRoom]> {
-        networkService
-            .requestTestChatRooms()
-            .map { data -> [ChatRoom] in
-                data.compactMap { $0.toEntity() }
+        let userID = tokenStorage.get(.userID) ?? ""
+        let chatEndpoint = ChatEndpoint.chatRooms(userID: userID)
+    
+        return networkService.request(chatEndpoint, interceptor: AuthInterceptor())
+            .decode(type: [ChatRoomDTO].self, decoder: JSONDecoder())
+            .map { chatRoomDTOs in
+                chatRoomDTOs.map { $0.toEntity() }
             }
     }
     
-    func leaveChatRoom(id: String) -> Single<Void> {
-        networkService
-            .leaveChatRoom(id: id)
+    func leaveChatRoom(id: String) -> Observable<Void> {
+        let chatEndpoint = ChatEndpoint.leaveChatRoom(chatRoomID: id)
+        
+        return networkService.request(chatEndpoint, interceptor: AuthInterceptor())
+            .map { _ in }
     }
 }
---
+++ Bridge/Sources/Data/Repositories/Chat/MockChatRoomRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MockChatRoomRepository.swift
+//  Bridge
+//
+//  Created by 정호윤 on 10/11/23.
+//
+
+import RxSwift
+
+final class MockChatRoomRepository: ChatRoomRepository {
+    func fetchChatRooms() -> Observable<[ChatRoom]> {
+//        .just(ChatRoomDTO.testArray.map { $0.toEntity() })
+        .error(NetworkError.unauthorized)
+    }
+    
+    func leaveChatRoom(id: String) -> Observable<Void> {
+        Observable.create { observer in
+            if let index = ChatRoomDTO.testArray.firstIndex(where: { $0.id == id }) {
+                ChatRoomDTO.testArray.remove(at: index)
+                observer.onNext(())
+            } else {
+                observer.onError(NetworkError.unknown)
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
