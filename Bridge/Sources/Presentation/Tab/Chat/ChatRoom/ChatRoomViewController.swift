a5af53f82a2e1f8593508a6f9c27f6b2ec2e0b0e
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-17 16:21:38 +0900

    feat: 뷰컨 UI 구현 (임시)
    #85

--- Bridge/Sources/Presentation/Tab/Chat/ChatRoom/ChatRoomViewController.swift
+++ Bridge/Sources/Presentation/Tab/Chat/ChatRoom/ChatRoomViewController.swift
@@ -6,8 +6,25 @@
 //
 
 import UIKit
+import FlexLayout
+import PinLayout
+import RxCocoa
+import RxSwift
+import Starscream
 
 final class ChatRoomViewController: BaseViewController {
+    // MARK: - UI
+    private let rootFlexConatiner = UIView()
+    
+    private let messageTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .black
+        textView.textColor = .white
+        textView.isEditable = false
+        return textView
+    }()
+    
+    private let messageInputView = BridgeMessageInputView()
 
     private let viewModel: ChatRoomViewModel
     
@@ -16,8 +33,43 @@ final class ChatRoomViewController: BaseViewController {
         super.init()
     }
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: - Configuration
+    override func configureAttributes() {
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    // MARK: - Layout
+    override func configureLayouts() {
+        view.addSubview(rootFlexConatiner)
+        
+        let margin: CGFloat = 16
+        
+        rootFlexConatiner.flex.define { flex in
+            flex.addItem(messageTextView).height(300).marginHorizontal(margin).marginVertical(20)
+            
+            flex.addItem(messageInputView)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        rootFlexConatiner.pin.all(view.pin.safeArea)
+        rootFlexConatiner.flex.layout()
+    }
+    
+    // MARK: - Binding
+    override func bind() {
+        let input = ChatRoomViewModel.Input(sendMessage: messageInputView.sendMessage)
+        _ = viewModel.transform(input: input)
     }
 }
