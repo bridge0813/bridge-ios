ec829a82d15363fcb4aa69591e8f24481c59d55b
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-10-24 23:52:32 +0900

    design: AddTechStackButton 구현
    #87

---
+++ Bridge/Sources/Presentation/Tab/Main/CreateProject/MemberDetailInput/AddTechStackButton.swift
@@ -0,0 +1,59 @@
+//
+//  AddTechStackButton.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/10/24.
+//
+
+import UIKit
+
+/// 팀원의 기술스택을 추가하는 버튼(스택이 추가되면, 타이틀이 수정으로 변경됨)
+final class AddTechStackButton: BaseButton {
+    
+    var isAdded: Bool = false {
+        willSet {
+            if newValue { updateConfigurationForEdit() }
+            else { updateConfigurationForAdd() }
+        }
+    }
+    
+    override func configureAttributes() {
+        updateConfigurationForAdd()
+    }
+    
+    // 추가된 스택이 없을 경우
+    private func updateConfigurationForAdd() {
+        let buttonImage = UIImage(named: "plus")?
+            .resize(to: CGSize(width: 14, height: 14))
+            .withRenderingMode(.alwaysTemplate)
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .clear
+        configuration.baseForegroundColor = BridgeColor.primary1
+    
+        configuration.image = buttonImage
+        configuration.imagePlacement = .leading
+        configuration.imagePadding = 2
+        
+        var titleContainer = AttributeContainer()
+        titleContainer.font = BridgeFont.body1.font
+        titleContainer.foregroundColor = BridgeColor.primary1
+        configuration.attributedTitle = AttributedString("추가", attributes: titleContainer)
+        
+        self.configuration = configuration
+    }
+    
+    // 추가된 스택이 있을 경우
+    private func updateConfigurationForEdit() {
+        var configuration = UIButton.Configuration.filled()
+        configuration.baseBackgroundColor = .clear
+        configuration.baseForegroundColor = BridgeColor.primary1
+    
+        var titleContainer = AttributeContainer()
+        titleContainer.font = BridgeFont.body1.font
+        titleContainer.foregroundColor = BridgeColor.primary1
+        configuration.attributedTitle = AttributedString("수정", attributes: titleContainer)
+        
+        self.configuration = configuration
+    }
+}
