3fc669615552d809e72729e16b5dafa317bf0234
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-10-28 16:56:56 +0900

    comment: 주석 수정

--- Bridge/Sources/Presentation/Common/Extensions/UIViewController+Keyboard.swift
+++ Bridge/Sources/Presentation/Common/Extensions/UIViewController+Keyboard.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     /// 화면의 빈 공간을 터치해 키보드를 가리고 싶은 경우, 필요한 뷰 컨트롤러에서 메서드 호출.
-    /// 사용 시, tap gesture recognizer간 충돌이 있을 수 있으므로 주의해서 사용해야함.
+    /// 사용 시  tap gesture recognizer간 충돌 발생하지 않도록 주의해서 사용해야함.
     func enableKeyboardHiding() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tapGestureRecognizer)
