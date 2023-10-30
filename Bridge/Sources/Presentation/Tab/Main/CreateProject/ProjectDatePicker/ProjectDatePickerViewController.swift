35d3c29ad9f03e3fe578d0104b4cedb8e9462e4b
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-10-31 02:47:33 +0900

    design: ProjectDatePickerViewController UI 구현
    #87

--- Bridge/Sources/Presentation/Tab/Main/CreateProject/ProjectDatePicker/ProjectDatePickerViewController.swift
+++ Bridge/Sources/Presentation/Tab/Main/CreateProject/ProjectDatePicker/ProjectDatePickerViewController.swift
@@ -12,31 +12,55 @@ import RxCocoa
 import RxSwift
 
 final class ProjectDatePickerViewController: BaseViewController {
-    // MARK: - Properties
+    // MARK: - UI
     private let rootFlexContainer = UIView()
-   
-    private let instructionLabel: UILabel = {
+    
+    private let progressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.progress = 0.6
+        progressView.progressTintColor = BridgeColor.primary1
+        progressView.backgroundColor = BridgeColor.gray7
+        
+        return progressView
+    }()
+    
+    private let descriptionLabel: UILabel = {
         let label = UILabel()
-        label.configureLabel(
-            textColor: .black,
-            font: .boldSystemFont(ofSize: 18),
-            numberOfLines: 2
-        )
-        label.text = "당신의 프로젝트에 대한 \n기본 정보를 알려주세요!(모집마감일, 시작일, 완료일 설정)"
+        label.configureTextWithLineHeight(text: "당신의 프로젝트에 대한\n기본 정보를 알려주세요!", lineHeight: 30)
+        label.font = BridgeFont.headline1Long.font
+        label.textColor = BridgeColor.gray1
+        label.numberOfLines = 2
         
         return label
     }()
     
-    private let nextButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("다음", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.setTitleColor(.white, for: .highlighted)
-        button.backgroundColor = .darkGray
+    private let deadlineLabel: UILabel = {
+        let label = UILabel()
+        label.text = "모집 마감일"
+        label.font = BridgeFont.subtitle2.font
+        label.textColor = BridgeColor.gray1
         
-        return button
+        return label
     }()
-
+    private let setDeadlineButton = BridgeSetDisplayButton("프로젝트 모집 마감일은 언제인가요?")
+    
+    private let startEndDateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "시작일과 완료일"
+        label.font = BridgeFont.subtitle2.font
+        label.textColor = BridgeColor.gray1
+        
+        return label
+    }()
+    private let setStartEndDateButton = BridgeSetDisplayButton("프로젝트 시작일과 예상 완료일은 언제인가요?")
+    
+    private let nextButton = BridgeButton(
+        title: "다음",
+        font: BridgeFont.button1.font,
+        backgroundColor: BridgeColor.gray4
+    )
+    
+    // MARK: - Properties
     private let viewModel: ProjectDatePickerViewModel
     
     // MARK: - Initializer
@@ -45,43 +69,46 @@ final class ProjectDatePickerViewController: BaseViewController {
         super.init()
     }
     
+    
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        configureNavigationUI()
     }
     
     override func viewDidLayoutSubviews() {
-        rootFlexContainer.pin.all(view.pin.safeArea).marginTop(10)
+        rootFlexContainer.pin.all(view.pin.safeArea)
         rootFlexContainer.flex.layout()
     }
     
-    // MARK: - Methods
-    private func configureNavigationUI() {
-    }
-    
+    // MARK: - Layout
     override func configureLayouts() {
         view.addSubview(rootFlexContainer)
-        rootFlexContainer.flex.direction(.column).padding(5).define { flex in
-            flex.addItem(instructionLabel).marginHorizontal(10).marginTop(20)
+        
+        rootFlexContainer.flex.paddingHorizontal(16).define { flex in
+            flex.addItem(progressView).height(6).marginTop(10)
+            flex.addItem(descriptionLabel).width(190).height(60).marginTop(40)
+            
+            flex.addItem(deadlineLabel).width(73).height(24).marginTop(40)
+            flex.addItem(setDeadlineButton).height(52).marginTop(14)
+            
+            flex.addItem(startEndDateLabel).width(101).height(24).marginTop(32)
+            flex.addItem(setStartEndDateButton).height(52).marginTop(14)
             
             flex.addItem().grow(1)
-            
-            flex.addItem().marginBottom(50).define { flex in
-                flex.addItem(nextButton).marginHorizontal(15).height(50).cornerRadius(8)
-            }
+            flex.addItem(nextButton).height(52).marginBottom(24)
         }
     }
     
+    // MARK: - Configure
     override func configureAttributes() {
         configureNavigationUI()
     }
     
+    private func configureNavigationUI() {
+        navigationItem.title = "모집글 작성"
+    }
+    
+    // MARK: - Bind
     override func bind() {
         let input = ProjectDatePickerViewModel.Input(
             nextButtonTapped: nextButton.rx.tap.asObservable(),
@@ -91,22 +118,5 @@ final class ProjectDatePickerViewController: BaseViewController {
         )
         let output = viewModel.transform(input: input)
         
-        output.dueDate
-            .drive(onNext: { date in
-                // 선택된 Date에 맞게 화면 업데이트
-            })
-            .disposed(by: disposeBag)
-        
-        output.startDate
-            .drive(onNext: { date in
-                // 선택된 Date에 맞게 화면 업데이트
-            })
-            .disposed(by: disposeBag)
-        
-        output.endDate
-            .drive(onNext: { date in
-                // 선택된 Date에 맞게 화면 업데이트
-            })
-            .disposed(by: disposeBag)
     }
 }
