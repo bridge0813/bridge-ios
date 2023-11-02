678a94b672d2f0a3a23ad9c28127fe3f35eb5c8d
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-11-03 02:28:41 +0900

    comment: 주석 추가
    #87

--- Bridge/Sources/Utils/Date+.swift
+++ Bridge/Sources/Utils/Date+.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 extension Date {
+    /// Date 타입을 원하는 포맷에 맞게 String 타입으로 전환
     func toString(format: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
@@ -15,6 +16,7 @@ extension Date {
         return dateFormatter.string(from: self)
     }
     
+    /// 현재  Date로부터 1년 후의 Date를 계산해주는 메서드
     func calculateMaximumDate() -> Date {
         let calendar = Calendar.current
         guard let maximumDate = calendar.date(byAdding: .year, value: 1, to: self) else { return Date() }
