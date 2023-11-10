0bc86e32d56c5bbfb0c7d88eebf14765f627d39c
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-11-10 21:49:12 +0900

    chore: 충돌 해결
    #92

--- Bridge/Sources/Presentation/DesignSystem/Component/Alert/Alertable.swift
+++ Bridge/Sources/Presentation/DesignSystem/Component/Alert/Alertable.swift
@@ -10,11 +10,15 @@ import UIKit
 /// 알림창의 메인 액션을 정의하는 클로저 (e.g. 신고하기 등)
 typealias PrimaryActionClosure = () -> Void
 
+/// 알림창의 취소 액션을 정의하는 클로저
+typealias CancelActionClosure = () -> Void
+
 protocol Alertable: AnyObject {
     func showAlert(
         target: UIViewController,
         configuration: AlertConfiguration,
-        primaryAction: PrimaryActionClosure?
+        primaryAction: PrimaryActionClosure?,
+        cancelAction: CancelActionClosure?
     )
     
     func showErrorAlert(
@@ -25,7 +29,12 @@ protocol Alertable: AnyObject {
 }
 
 extension Alertable {
-    func showAlert(target: UIViewController, configuration: AlertConfiguration, primaryAction: PrimaryActionClosure?) {
+    func showAlert(
+        target: UIViewController,
+        configuration: AlertConfiguration,
+        primaryAction: PrimaryActionClosure?,
+        cancelAction: CancelActionClosure?
+    ) {
         let alertViewController = BridgeAlertViewController(configuration: configuration, primaryAction: primaryAction)
         alertViewController.modalPresentationStyle = .overFullScreen
         alertViewController.modalTransitionStyle = .crossDissolve
