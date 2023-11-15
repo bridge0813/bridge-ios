a5ac2b6ae9da3f6d810aea59e281a15bdd9d453b
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-11-16 02:03:31 +0900

    feat: 포맷 형식 수정
    #93

--- Bridge/Sources/Data/Network/DTOs/ProjectDetail/ProjectDetailDTO.swift
+++ Bridge/Sources/Data/Network/DTOs/ProjectDetail/ProjectDetailDTO.swift
@@ -40,10 +40,10 @@ extension ProjectDetailDTO {
         ProjectDetail(
             title: title,
             description: description,
-            dDays: Date().calculateDDay(to: deadline.toDate(with: "yyyy.MM.dd")),
-            deadline: deadline.toDate(with: "yyyy.MM.dd"),
-            startDate: startDate?.toDate(with: "yyyy.MM.dd"),
-            endDate: endDate?.toDate(with: "yyyy.MM.dd"),
+            dDays: Date().calculateDDay(to: deadline.toDate(with: "yyyy-MM-dd'T'HH:mm:ss")),
+            deadline: deadline.toDate(with: "yyyy-MM-dd'T'HH:mm:ss"),
+            startDate: startDate?.toDate(with: "yyyy-MM-dd'T'HH:mm:ss"),
+            endDate: endDate?.toDate(with: "yyyy-MM-dd'T'HH:mm:ss"),
             memberRequirements: memberRequirements.map { $0.toEntity() },
             applicantRestrictions: applicantRestrictions,
             progressMethod: progressMethod,
@@ -68,9 +68,9 @@ extension ProjectDetailDTO {
     static var projectDetailTest = ProjectDetailDTO(
         title: "사이드 프젝으로 IOS앱을 같이 구현할 팀원을 구하고 있어요~",
         description: "안녕하세요 기획부터 앱 출시까지 함께하실 팀원을 모집합니다! 현재 인원은 총 3명입니다. 저희는 개발자, 디자이너, 기횟들이 프로젝트나 스터디 모집을 쉽고 효과적으로 할 수 있게 돕는 IOS앱입니다. 현재는 다른 플랫폼과 차별화된 기능에 대한 기획정도만 마쳐진 상황이며, 추가적으로 더 진행될 예정입니다!",
-        deadline: "2023.11.27",
-        startDate: "2023.12.01",
-        endDate: "2024.04.25",
+        deadline: "2023-11-27T21:37:19",
+        startDate: "2023-12-01T21:37:19",
+        endDate: "2024-04-01T21:37:19",
         memberRequirements: [
             MemberRequirementDTO(field: "ios", recruitNumber: 2, requiredSkills: ["Swift", "UIKit", "SwiftUI", "RxSwift", "RxCocoa"], requirementText: "스위프트 사용에 익숙하신 분이었으면 좋겠습니다."),
             MemberRequirementDTO(field: "uiux", recruitNumber: 2, requiredSkills: ["photoshop", "Figma", "illustrator"], requirementText: "피그마 사용에 능숙했으면 좋겠습니다."),
