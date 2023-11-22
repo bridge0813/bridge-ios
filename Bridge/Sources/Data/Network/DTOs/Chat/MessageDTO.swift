adfc2129c5a673a3b2adcad441321e219019d6a5
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-11-22 12:18:32 +0900

    fix: DTO 수정
    #94

--- Bridge/Sources/Data/Network/DTOs/Chat/MessageDTO.swift
+++ Bridge/Sources/Data/Network/DTOs/Chat/MessageDTO.swift
@@ -10,7 +10,7 @@ import Foundation
 /// 채널의 기존 메시지들을 불러오기 위한 타입
 struct MessageDTO: Decodable {
     let messageID: String
-    let senderID: String
+    let senderID: Int
     let type: String
     let content: String
     let sentDateAndTime: String
@@ -40,7 +40,7 @@ extension MessageDTO {
     func toEntity(userID: String) -> Message {
         Message(
             id: messageID,
-            sender: userID == senderID ? .me : .opponent,
+            sender: Int(userID) == senderID ? .me : .opponent,
             type: mapToMessageType(type),
             sentDate: sentDateAndTime.toDate() ?? "",
             sentTime: sentDateAndTime.toSimpleTime() ?? "",
@@ -66,7 +66,7 @@ extension MessageDTO {
     static var testArray = [
         MessageDTO(
             messageID: UUID().uuidString,
-            senderID: "1",
+            senderID: 1,
             type: "TALK",
             content: "안녕하세요",
             sentDateAndTime: "2023-11-13T18:18:00",
@@ -74,7 +74,7 @@ extension MessageDTO {
         ),
         MessageDTO(
             messageID: UUID().uuidString,
-            senderID: "1",
+            senderID: 1,
             type: "TALK",
             content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
             sentDateAndTime: "2023-11-14T18:20:00",
@@ -82,7 +82,7 @@ extension MessageDTO {
         ),
         MessageDTO(
             messageID: UUID().uuidString,
-            senderID: "1",
+            senderID: 1,
             type: "TALK",
             content: "안녕하세요 반갑습니다!",
             sentDateAndTime: "2023-11-15T18:20:00",
@@ -90,7 +90,7 @@ extension MessageDTO {
         ),
         MessageDTO(
             messageID: UUID().uuidString,
-            senderID: "1",
+            senderID: 1,
             type: "REJECT",
             content: "안녕하세요 반갑습니다!",
             sentDateAndTime: "2023-11-15T18:20:00",
@@ -98,7 +98,7 @@ extension MessageDTO {
         ),
         MessageDTO(
             messageID: UUID().uuidString,
-            senderID: "1",
+            senderID: 1,
             type: "ACCEPT",
             content: "안녕하세요 반갑습니다!",
             sentDateAndTime: "2023-11-15T18:20:00",
