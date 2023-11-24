bceed73145ff0258eabb2453c8a37e261b1eb4d6
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-11-25 00:56:32 +0900

    feat: ProfileEndpoint 구현
    #99

---
+++ Bridge/Sources/Data/Network/Endpoint/Profile/ProfileEndpoint.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileEndpoint.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/11/25.
+//
+
+import Foundation
+
+enum ProfileEndpoint {
+    case profilePreview(userId: Int)
+}
+
+extension ProfileEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .profilePreview:
+            return "/users/mypage"
+        }
+    }
+    
+    var queryParameters: QueryParameters? {
+        switch self {
+        case .profilePreview(let userId):
+            return ["userId": String(userId)]
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .profilePreview:
+            return .get
+        }
+    }
+    
+    var body: Encodable? { nil }
+}
