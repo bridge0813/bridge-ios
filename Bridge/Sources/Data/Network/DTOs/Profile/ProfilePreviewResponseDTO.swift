2f6bf2d211fdce5a08956f521470b5ddffc7ea90
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-11-25 00:27:57 +0900

    feat: ProfilePreviewResponseDTO 구현
    #99

---
+++ Bridge/Sources/Data/Network/DTOs/Profile/ProfilePreviewResponseDTO.swift
@@ -0,0 +1,19 @@
+//
+//  ProfilePreviewResponseDTO.swift
+//  Bridge
+//
+//  Created by 엄지호 on 2023/11/24.
+//
+
+/// MyPage를 조회할 때 사용되는 Response Body
+struct ProfilePreviewResponseDTO: Decodable {
+    let profileImage: String
+    let field: [String]
+    let bookmarkNumber: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case field
+        case profileImage = "profilePhoto"
+        case bookmarkNumber = "bookmarkNum"
+    }
+}
