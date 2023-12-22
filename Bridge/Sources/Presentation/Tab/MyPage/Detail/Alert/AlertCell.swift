616b6feb052655619ac49833f7461556565a41bf
Hoyun Jung <65343417+stemmmm@users.noreply.github.com>
2023-12-22 11:38:01 +0900

    design: 알림 UI 구현
    #106

---
+++ Bridge/Sources/Presentation/Tab/MyPage/Detail/Alert/AlertCell.swift
@@ -0,0 +1,62 @@
+//
+//  AlertCell.swift
+//  Bridge
+//
+//  Created by 정호윤 on 12/22/23.
+//
+
+import UIKit
+import FlexLayout
+import PinLayout
+
+final class AlertCell: BaseTableViewCell {
+    // MARK: - UI
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = BridgeFont.body1.font
+        label.textColor = BridgeColor.gray01
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = BridgeFont.body2Long.font
+        label.textColor = BridgeColor.gray02
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = BridgeFont.caption1.font
+        label.textColor = BridgeColor.gray04
+        return label
+    }()
+    
+    // MARK: - Layout
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.pin.all()
+        contentView.flex.layout()
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        contentView.pin.width(size.width)
+        contentView.flex.layout()
+        return contentView.frame.size
+    }
+    
+    override func configureLayouts() {
+        contentView.flex.padding(20, 16).define { flex in
+            flex.addItem(titleLabel)
+            flex.addItem(descriptionLabel).marginVertical(4)
+            flex.addItem(dateLabel)
+        }
+    }
+}
+
+// MARK: - Configuration
+extension AlertCell {
+    func configure() {
+        
+    }
+}
--- Bridge/Sources/Presentation/Tab/MyPage/Detail/Alert/AlertViewController.swift
+++ Bridge/Sources/Presentation/Tab/MyPage/Detail/Alert/AlertViewController.swift
@@ -6,13 +6,33 @@
 //
 
 import UIKit
+import FlexLayout
+import PinLayout
+import RxCocoa
+import RxSwift
 
 final class AlertViewController: BaseViewController {
     // MARK: - UI
+    private let removeAllButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전체 삭제", for: .normal)
+        button.setTitleColor(BridgeColor.primary1, for: .normal)
+        button.titleLabel?.font = BridgeFont.body2.font
+        return button
+    }()
     
+    private lazy var alertListTableView: UITableView = {
+        let tableView = UITableView()
+//        tableView.register()
+        tableView.rowHeight = 120
+        tableView.separatorStyle = .singleLine
+        tableView.separatorInset = .zero
+        return tableView
+    }()
     
     // MARK: - Property
     private let viewModel: AlertViewModel
+    private let removeAlert = PublishRelay<Int>()
     
     // MARK: - Init
     init(viewModel: AlertViewModel) {
@@ -25,19 +45,52 @@ final class AlertViewController: BaseViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
     // MARK: - Configuration
     override func configureAttributes() {
         navigationItem.title = "알림"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: removeAllButton)
     }
     
     // MARK: - Layout
     override func configureLayouts() {
+        view.addSubview(alertListTableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
+        alertListTableView.pin.all(view.pin.safeArea)
+        alertListTableView.flex.layout()
     }
     
     // MARK: - Binding
     override func bind() {
         let input = AlertViewModel.Input()
         let output = viewModel.transform(input: input)
+    }
+}
+
+// MARK: - Delegate
+extension AlertViewController: UITableViewDelegate {
+    func tableView(
+        _ tableView: UITableView,
+        trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath
+    ) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "삭제") { [weak self] _, _, completion in
+            self?.removeAlert.accept(indexPath.row)
+            completion(true)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }
