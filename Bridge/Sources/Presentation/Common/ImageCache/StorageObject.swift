afdb23d4e1c5b64ab82db6e9d9f6b5d87d478584
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-01-28 02:12:49 +0900

    feat: 저장할 객체의 만료일 정의
    #121

--- Bridge/Sources/Presentation/Common/ImageCache/StorageObject.swift
+++ Bridge/Sources/Presentation/Common/ImageCache/StorageObject.swift
@@ -10,8 +10,23 @@ import Foundation
 // 메모리와 디스크에 저장할 객체
 final class StorageObject {
     let imageData: Data
+    private let expiration: StorageExpiration
+    var estimatedExpiration: Date  // 만료 날짜
     
-    init(imageData: Data) {
+    init(imageData: Data, expiration: StorageExpiration) {
         self.imageData = imageData
+        self.expiration = expiration
+        self.estimatedExpiration = expiration.estimatedExpirationSince()
+    }
+    
+    var isExpired: Bool {
+        return estimatedExpiration.isPast
+    }
+}
+
+extension Date {
+    /// 현재 시간과 비교하여, 과거의 날짜(만료됨)인지 여부를 반환
+    var isPast: Bool {
+        return timeIntervalSince(Date()) <= 0
     }
 }
