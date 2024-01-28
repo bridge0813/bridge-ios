4304216a3a1633204ac5eea57800d3fe0a4fa3e8
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-01-28 13:49:15 +0900

    feat: setImage 구현
    #121

--- Bridge/Sources/Presentation/Common/Extensions/UIImageView+KingFisher.swift
+++ Bridge/Sources/Presentation/Common/Extensions/UIImageView+KingFisher.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 extension UIImageView {
     /// URL을 가지고 이미지를 다운로드하여 적용하는 메서드
@@ -15,31 +14,22 @@ extension UIImageView {
     /// - Parameter size: 다운샘플링 할 이미지의 크기
     /// - Parameter placeholderImage: 이미지 적용에 실패했을 경우, 적용되는 플레이스홀더 이미지
     func setImage(
-        with urlString: URLString?,
+        from urlString: URLString?,
         size: CGSize,
         placeholderImage: UIImage? = UIImage(named: "profile.small")
     ) {
-        guard let urlString else { return self.image = placeholderImage }
-        let url = URL(string: urlString)
+        // 유효하지 않은 URL인 경우 플레이스홀더를 이미지로 설정
+        guard let urlString, let url = URL(string: urlString) else {
+            image = placeholderImage
+            return
+        }
         
-        // 다운샘플링 후 Radius 적용
-        let processor = DownsamplingImageProcessor(size: size)
-        |> RoundCornerImageProcessor(cornerRadius: self.layer.cornerRadius)
-        
-        // 이미지 처리 옵션
-        let options: KingfisherOptionsInfo = [
-            .scaleFactor(UIScreen.main.scale),
-            .processor(processor),
-            .transition(.fade(0.4)),
-            .cacheOriginalImage
-        ]
-        
-        // 다운로드 작업이 진행중일 때, 이미지 뷰에 인디케이터 설정.
-        self.kf.indicatorType = .activity
-        self.kf.setImage(with: url, options: options) { [weak self] result in
-            if case .failure(let error) = result {
-                print(error.localizedDescription)
-                self?.image = placeholderImage
+        ImageCache.shared.load(url: url, withOption: .downsampling(size: size)) { [weak self] image in
+            guard let self else { return }
+            
+            UIView.transition(with: self, duration: 0.3, options: .transitionCrossDissolve) {
+                // 이미지가 없으면 플레이스홀더 적용, 있으면 해당 이미지 적용
+                self.image = image == nil ? placeholderImage : image
             }
         }
     }
