f19f585a54c2c6428e3f573078f9c67cb715bb24
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-01-30 13:05:22 +0900

    chore: 채팅방 개설 DTO 수정
    #123

--- Bridge/Sources/Data/Network/DTOs/Chat/CreateChannelResponseDTO.swift
+++ Bridge/Sources/Data/Network/DTOs/Chat/CreateChannelResponseDTO.swift
@@ -10,12 +10,16 @@ import Foundation
 struct CreateChannelResponseDTO: Decodable {
     let channelID: String
     let myID: Int
+    let myProfileImageURL: String  // 사용안함(서버 측 JSON 구조 맞추기)
     let opponentID: Int
+    let opponentProfileImageURL: String
     
     enum CodingKeys: String, CodingKey {
         case channelID = "chatRoomId"
         case myID = "makeUserId"
+        case myProfileImageURL = "makeUserPhotoUrl"
         case opponentID = "receiveUserId"
+        case opponentProfileImageURL = "receiveUserPhotoUrl"
     }
 }
 
@@ -25,7 +29,7 @@ extension CreateChannelResponseDTO {
             id: channelID,
             myID: String(myID),
             opponentID: String(opponentID),
-            imageURL: nil,
+            imageURL: opponentProfileImageURL,
             name: "",
             lastMessage: Channel.LastMessage(
                 receivedTime: "",
@@ -37,5 +41,5 @@ extension CreateChannelResponseDTO {
 }
 
 extension CreateChannelResponseDTO {
-    static var testData = CreateChannelResponseDTO(channelID: "", myID: 2, opponentID: 1)
+    static var testData = CreateChannelResponseDTO(channelID: "", myID: 2, myProfileImageURL: "", opponentID: 1, opponentProfileImageURL: "https://firebasestorage.googleapis.com/v0/b/cookingproject-5bf82.appspot.com/o/saveImage1677219882.2262769?alt=media&token=12b9aab3-a0b0-498f-bc81-0e8a8858d481")
 }
