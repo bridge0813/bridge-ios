1ba02244ee618aa60534b23d897021ca0bd9e33e
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-02-11 17:50:41 +0900

    feat: 모집글 수정 유스케이스 구현
    #129

--- Bridge/Sources/Domain/RepositoryInterfaces/Project/ProjectRepository.swift
+++ Bridge/Sources/Domain/RepositoryInterfaces/Project/ProjectRepository.swift
@@ -8,27 +8,42 @@
 import RxSwift
 
 protocol ProjectRepository {
-    // MARK: - Fetch
+    // 모든 모집글 조회
     func fetchAllProjects() -> Observable<[ProjectPreview]>
+    
+    // 분야에 맞는 모집글 조회
     func fetchProjectsByField(for field: String) -> Observable<[ProjectPreview]>
+    
+    // 인기 모집글 조회
     func fetchHotProjects() -> Observable<[ProjectPreview]>
+    
+    // 마감임박 모집글 조회
     func fetchDeadlineProjects() -> Observable<[ProjectPreview]>
+    
+    // 모집글 상세 조회
     func fetchProjectDetail(with projectID: Int) -> Observable<Project>
     
+    // 내가 지원한 모집글 조회
     func fetchAppliedProjects() -> Observable<[ProjectPreview]>
+    
+    // 내가 작성한 모집글 조회
     func fetchMyProjects() -> Observable<[ProjectPreview]>
     
+    // 필터링된 모집글 조회
     func fetchFilteredProjects(filterBy fieldTechStack: FieldTechStack) -> Observable<[ProjectPreview]>
     
-    // MARK: - Create
+    // 모집글 작성
     func create(project: Project) -> Observable<Int>
     
-    // MARK: - Bookmark
+    // 모집글 수정
+    func update(project: Project) -> Observable<Void>
+    
+    // 북마크
     func bookmark(projectID: Int) -> Observable<Int>
     
-    // MARK: - Delete
+    // 모집글 제거
     func delete(projectID: Int) -> Observable<Int>
     
-    // MARK: - Close
+    // 모집글 마감
     func close(projectID: Int) -> Observable<Int>
 }
