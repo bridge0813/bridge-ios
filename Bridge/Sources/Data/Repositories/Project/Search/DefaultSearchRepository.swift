81f32e949d390b401f84639c2f1449714a8d21b2
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-02-16 21:41:08 +0900

    feat: 검색 API 수정
    #129

--- Bridge/Sources/Data/Repositories/Project/Search/DefaultSearchRepository.swift
+++ Bridge/Sources/Data/Repositories/Project/Search/DefaultSearchRepository.swift
@@ -37,7 +37,8 @@ final class DefaultSearchRepository: SearchRepository {
     }
     
     func searchProjects(by query: String) -> Observable<[ProjectPreview]> {
-        let searchProjectDTO = SearchProjectRequestDTO(searchWord: query)
+        let userID = tokenStorage.get(.userID)
+        let searchProjectDTO = SearchProjectRequestDTO(userID: Int(userID) ?? -1, searchWord: query)
         let searchProjectEndpoint = SearchEndpoint.searchProjects(requestDTO: searchProjectDTO)
         
         return networkService.request(to: searchProjectEndpoint, interceptor: nil)
